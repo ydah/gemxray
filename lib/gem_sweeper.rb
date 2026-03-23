113c7ead6f441dfaf9408071f75b0201dc430f20
ydah <t.yudai92@gmail.com>
2026-03-23 13:02:51 +0900

    Initial commit

---
+++ lib/gem_sweeper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gem_sweeper/version"
+
+module GemSweeper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/gem_sweeper/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module GemSweeper
+  VERSION = "0.1.0"
+end
